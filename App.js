ef3b7252e98f0eedc0f1225712a1aefae764ea09
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2024-01-03 07:00:48 +0900

    16

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of Text!</Text>
+        <Text style={styles.dummyText}>Another piece of Text!</Text>
       </View>
-      <Text style={{ margin: 16, borderWidth: 2, borderColor: 'red', padding: 16 }}>Hello World!</Text>
+      <Text style={styles.dummyText}>Hello World!</Text>
       <Button title='Tap Me!' />
     </View>
   );
@@ -19,4 +19,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue'
+  }
 });
